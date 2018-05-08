04e1bcdd2b1be558bcd6177035fb4441451aedf0
Dusan Orlovic <duleorlovic@gmail.com>
2018-05-08 14:25:48 +0200

    Move step duration to params

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 var FLASH_LETTER_STEP = 10;
 var FLASH_DURATION = 5000;
 
-export function element_text(element, text, duration) {
+export function element_text(element, text, duration, letter_step) {
   duration = duration || FLASH_DURATION;
-  flash_appear(element, text);
-  setTimeout(function(){ flash_dissapear(element); }, duration);
+  letter_step = leter_step || FLASH_LETTER_STEP;
+  flash_appear(element, text, letter_step);
+  setTimeout(function(){ flash_dissapear(element, letter_step); }, duration);
 }
 
-function flash_appear(element, text, i) {
+function flash_appear(element, text, i, letter_step) {
   if (i == undefined)
     i = 0;
   element.innerText = text.substring(0,i);
   setTimeout(function(){
     if (i<text.length)
-      flash_appear(element, text, i+1);
-  }, FLASH_LETTER_STEP);
+      flash_appear(element, text, i+1, letter_step);
+  }, letter_step);
 }
 
-function flash_dissapear(element, text, i) {
+function flash_dissapear(element, text, i, letter_step) {
   if (text == undefined)
     text = element.innerText;
   if (i == undefined)
@@ -25,6 +26,6 @@ function flash_dissapear(element, text, i) {
   element.innerText = text.substring(0,i);
   setTimeout(function(){
     if (i>0)
-      flash_dissapear(element, text, i-1);
-  }, FLASH_LETTER_STEP);
+      flash_dissapear(element, text, i-1, letter_step);
+  }, letter_step);
 }
